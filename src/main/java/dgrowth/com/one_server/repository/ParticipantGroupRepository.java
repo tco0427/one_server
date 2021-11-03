3388b104eabf813dc0c12c41a51df8e129d7e51b
Donggyu Kim <a29661498@gmail.com>
2021-11-03 23:55:57 +0900

    fix(hot): 실시간 핫그룹 조회 기능 수정
    * JPAQueryFactory 초기 설정 추가
    * JWT accessToken 유효시간 30분으로 변경

--- src/main/java/dgrowth/com/one_server/repository/ParticipantGroupRepository.java
+++ src/main/java/dgrowth/com/one_server/repository/ParticipantGroupRepository.java
@@ -1,16 +1,11 @@
 package dgrowth.com.one_server.repository;
 
-import dgrowth.com.one_server.domain.entity.Group;
+
 import dgrowth.com.one_server.domain.entity.ParticipantGroup;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface ParticipantGroupRepository extends JpaRepository<ParticipantGroup, Long> {
